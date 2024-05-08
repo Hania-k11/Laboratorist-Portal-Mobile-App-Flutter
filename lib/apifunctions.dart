cc04b171a2a84bdc3723db2a8b83b1d6385ddfc7
Hania <haniaaa0987@example.com>
2024-05-09 00:25:15 +0500

    report by hania

---
+++ lib/apifunctions.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+var ip = "192.168.100.6";
+
+Future<void> fetch() async {
+  final url = Uri.parse('http://192.168.100.6:8000/getallhospitals/');
+  //final visit = Visit(doctorid: doctorid, date: date);
+  final response = await http.get(
+    url,
+    headers: {'Content-Type': 'application/json'},
+
+  );
+
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    print(jsonResponse.toString());
+  } else {
+    throw Exception('Failed to load data');
+  }
+
+
+
+
+}
+
+
+Future<void> fetchDataAndDisplay() async {
+  // Replace 'https://api.example.com/data' with the URL of your API endpoint
+  var url = Uri.parse('https://10.133.136.71:8000/getallhospitals');
+
+  try {
+    // Send a GET request to the API endpoint
+    var response = await HttpClient().getUrl(url);
+    print('i am here');
+    // Close the request, wait for the response, and decode JSON
+    var jsonResponse = jsonDecode(await (await response.close()).transform(utf8.decoder).join());
+
+    // Display the fetched data on the console
+    print(jsonResponse);
+  } catch (e) {
+    print('Error fetching data: $e');
+  }
+}
+class apifunctions{
+
+
+
+
+}
--- lib/report.dart
+++ lib/report.dart
@@ -7,51 +7,51 @@ class report extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 200, // Adjust the height of the image as needed
-              child: Image.asset(
-                'assets/images/report.png', // Replace 'your_image.jpg' with your image asset path
-                fit: BoxFit.contain,
-              ),
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/Group 89.png',
+              fit: BoxFit.contain,
+              height: MediaQuery.of(context).size.height * 0.8, // Adjust the size of the image
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Positioned(
+            left: 100,
+            top: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Zainab Rauf'),
-                SizedBox(width: 10),
-                Text('729200'),
+                Row(
+                  children: [
+                    Text('Zainab Rauf'),
+                    SizedBox(width: 10),
+                    Text('729200'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text('Female'),
+                    SizedBox(width: 10),
+                    Text('07/07/2024'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text('20'),
+                    SizedBox(width: 10),
+                    Text('686868'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text('Dr. Farheen'),
               ],
             ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Female'),
-                SizedBox(width: 10),
-                Text('07/07/2024'),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('20'),
-                SizedBox(width: 10),
-                Text('686868'),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text('Dr. Farheen'),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
